0b8b5af9cfb34471603a16022fa055f4f272a4a1
Tom Airey <aireytf19@gmail.com>
2024-01-16 16:46:05 +0000

    add initial questions.js, with test Qs

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+var questions = [
+    // Test questions
+    {
+        title: "Commonly used data types DO NOT include:"
+        choices: ["strings", "booleans", "alerts", "numbers"]
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ______ ."
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"]
+    }
+]
