6075b2f64f15ec1b53faa7084ef0fe9ca9922f22
Tom Airey <aireytf19@gmail.com>
2024-01-16 16:50:25 +0000

    add DOM elements, as per IDs in starter html

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+// DOM elements
+const startBtn = document.getElementById("start");
+const timerElement = document.getElementById("time");
+const questionTitleElement = document.getElementById("question-title");
+const choicesElement = document.getElementById("choices");
+const feedbackElement = document.getElementById("feedback");
+const endScreenElement = document.getElementById("end-screen");
+const initialsInput = document.getElementById("initials");
+const submitBtn = document.getElementById("submit");
+
